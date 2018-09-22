b36730558e787fa07cb6b3d66ebdb4e2cf50a9fe
Parth Patel <parthcdgsa@gmail.com>
2018-09-22 20:39:36 +0530

    finished project

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
-$(".del-item").on("click", function (e) {
+$("ul").on("click",".del-item", function (e) {
     $(this).css("background-color", "#232323");
+    $(this).parent("li").fadeOut(1000, function (e) {
+        $(this).remove();
+    });
+    e.stopPropagation();
 });
 
 
-$(".items").on("click", function (e) {
-   $(this).toggleClass("tasks-done");
-})
+$("ul").on("click",".items", function (e) {
+    $(this).toggleClass("tasks-done");
+});
+
+$("#todo-add").on("keypress",function(e){
+    if(e.which === 13){
+        // alert("Enter pressed!");   
+        console.log($(this).val());
+        $("ul").append('<li class="items"><span class="del-item"><i class="far fa-trash-alt"></i></span> '  + $(this).val() + "</li>");
+        $(this).val("");
+    }
+});
